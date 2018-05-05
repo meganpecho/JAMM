5c8c94bc5020714ce592de73c6eaf2b8a98253b4
Jeremy Babicz <cabbagebabicz@gmail.com>
2018-05-05 17:38:26 -0500

    daily to do tasks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { DailyToDoComponent } from './components/daily-to-do/daily-to-do.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DailyToDoComponent
   ],
   imports: [
     BrowserModule
