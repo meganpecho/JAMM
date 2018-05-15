0ff886d5338d53966f8dbbd217929811c707c4fa
Megan Pecho <mepecho@gmail.com>
2018-05-15 18:34:49 -0500

    Accidental reference to deleted component deleted

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './landing/landing.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { CallbackComponent } from './callback/callback.component';
 
 const routes:Routes = [
     { path: '', redirectTo: 'landing', pathMatch: 'full' },
     { path: 'landing', component: LandingComponent },
-    { path: 'callback', component: CallbackComponent },
     { path: 'dashboard', component: DashboardComponent },
     { path: '**', redirectTo: '' }
 ];
