1eebfd21ee35018aed4b291d3ff186ce0c7b7605
aerocat <mferri1@depaul.edu>
2018-05-27 23:23:20 -0500

    Able to display stuff from Firebase in the HTML template

--- src/app/time-tracker/time-tracker.component.ts
+++ src/app/time-tracker/time-tracker.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DbConnectService } from '../services/db-connect.service';
-
+import { Timestamp } from '../models/timestamp';
 
 @Component({
   selector: 'app-time-tracker',
@@ -8,12 +8,15 @@ import { DbConnectService } from '../services/db-connect.service';
   styleUrls: ['./time-tracker.component.scss']
 })
 export class TimeTrackerComponent implements OnInit {
+  // "timestamps" will be acccessible from the HTML template
+  timestamps: Timestamp[];
 
   constructor(private timestampService: DbConnectService) { }
 
   ngOnInit() {
     this.timestampService.getTimestamps().subscribe(timestamps => {
-        console.log(timestamps);
+        // console.log(timestamps);
+        this.timestamps = timestamps;
     });
   }
 
