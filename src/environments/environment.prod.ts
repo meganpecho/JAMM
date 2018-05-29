087af589f5c07ed7469b008bd9a5882f7768955d
Megan Pecho <mepecho@gmail.com>
2018-05-29 14:37:07 -0500

    Made this deployable on Heroku, edit prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   CLIENTID: '1xeExBwmulevpJB508a6w8Xq7LpdLMHC',
   DOMAIN: 'backlogjamm.auth0.com',
-  CALLBACKURL: 'http://localhost:4200/dashboard'
+  CALLBACKURL: 'https://backlogjamm.herokuapp.com/dashboard'
 };
