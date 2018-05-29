471a79b401f6380ae8f2da12662ce57d41bdcef1
Megan Pecho <mepecho@gmail.com>
2018-05-29 13:26:50 -0500

    Env set up

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  clientID: '1xeExBwmulevpJB508a6w8Xq7LpdLMHC',
+  domain: 'backlogjamm.auth0.com',
+  callbackURL: 'http://localhost:4200/dashboard'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,8 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  clientID: '1xeExBwmulevpJB508a6w8Xq7LpdLMHC',
+  domain: 'backlogjamm.auth0.com',
+  callbackURL: 'http://localhost:4200/dashboard'
 };
