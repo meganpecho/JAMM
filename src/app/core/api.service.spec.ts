ac9b332f8d178b74d2161ad5f1711be07ae46b5f
aerocat <mferri1@depaul.edu>
2018-06-06 00:01:47 -0500

    Added a few more tests for the API Service

--- src/app/core/api.service.spec.ts
+++ src/app/core/api.service.spec.ts
@@ -98,5 +98,73 @@ fdescribe('ApiService', () => {
 
   });
 
+  it('should retrieve tasks from the API via GET', () => {
+    const sampleResponse: Task[] = [
+      { "_id": "some_id",
+        "userId": "some_userId",
+        "name": "some_name",
+        "createdAtDate": new Date(),
+        "actualTime": 10, // some number
+        "estimatedTime": 15, // some number
+        "inProgress": false, // some boolean
+        "completed": false,  // some boolean
+      },
+      { "_id": "some_id2",
+        "userId": "some_userId2",
+        "name": "some_name2",
+        "createdAtDate": new Date(),
+        "actualTime": 15, // some number
+        "estimatedTime": 120, // some number
+        "inProgress": true, // some boolean
+        "completed": false,  // some boolean
+      }
+    ];
+    service.getTasks().subscribe(tasks => {
+      expect(tasks.length).toBe(2);
+      expect(tasks).toEqual(sampleResponse);
+    });
+
+    const sampleRequest = httpMock.expectOne(`${ENV.BASE_API}tasks`);
+
+    expect(sampleRequest.request.method).toBe('GET');
+
+    sampleRequest.flush(sampleResponse);
+
+  });
+
+  it('should retrieve tasks from the API via GET', () => {
+    const sampleResponse: Task[] = [
+      { "_id": "some_id",
+        "userId": "some_userId",
+        "name": "some_name",
+        "createdAtDate": new Date(),
+        "actualTime": 10, // some number
+        "estimatedTime": 15, // some number
+        "inProgress": false, // some boolean
+        "completed": false,  // some boolean
+      },
+      { "_id": "some_id2",
+        "userId": "some_userId2",
+        "name": "some_name2",
+        "createdAtDate": new Date(),
+        "actualTime": 15, // some number
+        "estimatedTime": 120, // some number
+        "inProgress": true, // some boolean
+        "completed": false,  // some boolean
+      }
+    ];
+    service.getTasks().subscribe(tasks => {
+      expect(tasks.length).toBe(2);
+      expect(tasks).toEqual(sampleResponse);
+    });
+
+    const sampleRequest = httpMock.expectOne(`${ENV.BASE_API}tasks`);
+
+    expect(sampleRequest.request.method).toBe('GET');
+
+    sampleRequest.flush(sampleResponse);
+
+  });
+
 
 });
